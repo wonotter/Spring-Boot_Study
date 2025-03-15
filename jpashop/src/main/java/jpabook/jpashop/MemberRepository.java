9017df18d651499bf421ec7a6847f76360b639a2
wonotter <zjaxhfl99@kumoh.ac.kr>
2025-03-15 17:28:30 +0900

    도메인 분석 설계 및 엔티티 클래스 개발

--- jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -7,15 +7,15 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
-
-    public Long save(Member member) {
-        em.persist(member);
-        return member.getId();
-    }
-
-    public Member find(Long id) {
-        return em.find(Member.class, id);
-    }
+//    @PersistenceContext
+//    private EntityManager em;
+//
+//    public Long save(Member member) {
+//        em.persist(member);
+//        return member.getId();
+//    }
+//
+//    public Member find(Long id) {
+//        return em.find(Member.class, id);
+//    }
 }
