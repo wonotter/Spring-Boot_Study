f868bb61acb0d0eb71cba1b958801f10cf27c9f8
wonotter <zjaxhfl99@kumoh.ac.kr>
2025-03-28 21:38:54 +0900

    웹 계층 개발
    홈 화면과 레이아웃, 회원 등록, 회원 목록 조회, 상품 등록, 상품 목록 조회

---
+++ jpashop/src/main/java/jpabook/jpashop/controller/BookController.java
@@ -0,0 +1,47 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class BookController {
+
+    private final ItemRepository itemRepository;
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String crateForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String crate(BookForm form) {
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
+}
