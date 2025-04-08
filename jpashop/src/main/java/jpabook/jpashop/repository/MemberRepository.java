59351699800e010396c3cdb5e0e9bf08a016f4d4
wonotter <zjaxhfl99@kumoh.ac.kr>
2025-04-08 21:32:44 +0900

    API 개발 고급 - 컬렉션 조회 최적화
    1. 주문 조회 V5: JPA에서 DTO로 직접 조회 - 컬렉션 조회 최적화
    
    2. 주문 조회 V6: JPA에서 DTO로 직접 조회 - 플랫 데이터 최적화
    
    3. OSIV와 성능 최적화

--- jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,42 +1,13 @@
 package jpabook.jpashop.repository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import jakarta.validation.constraints.NotEmpty;
 import jpabook.jpashop.domain.Member;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-@Repository
-@RequiredArgsConstructor
-public class MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    /**
-    * EntityManager 역시 @RequiredArgsConstructor 사용 가능
-    * Spring Data JPA가 이를 지원하기 때문
-     */
-//    @PersistenceContext
-//    private EntityManager em;
-
-    private final EntityManager em;
-
-    public void save(Member member) {
-        em.persist(member);
-    }
-
-    public Member findOne(Long id) {
-        return em.find(Member.class, id);
-    }
-
-    public List<Member> findAll() {
-        return em.createQuery("select m from Member m", Member.class)
-                .getResultList();
-    }
-
-    public List<Member> findByName(String name) {
-        return em.createQuery("select m from Member m where m.name = :name", Member.class)
-                .setParameter("name", name)
-                .getResultList();
-    }
+    // select m from Member m where m.name = ?
+    List<Member> findByName(String name);
 }
