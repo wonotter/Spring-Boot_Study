2b00c2b555f6f39899226d884ce8bae0cd3fd01c
wonotter <zjaxhfl99@kumoh.ac.kr>
2025-04-18 01:28:03 +0900

    쿼리 메서드 기능
    1. JPA 반환 타입
    2. 순수 JPA 페이징과 정렬

--- data-jpa/src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -11,6 +11,7 @@ import study.data_jpa.entity.Team;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -155,5 +156,17 @@ public class MemberRepositoryTest {
         for (Member member : result) {
             System.out.println("member = " + member);
         }
+    }
+
+    @Test
+    public void returnType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        Optional<Member> findMember = memberRepository.findOptionalByUsername("AAA");
+        System.out.println("findMember = " + findMember);
     }
 }
