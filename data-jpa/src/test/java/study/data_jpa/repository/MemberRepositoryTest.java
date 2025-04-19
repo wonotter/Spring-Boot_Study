493225f610324061d5a24d6bf223b07521d7b18f
Wonho Kim <wonho.kim99@gmail.com>
2025-04-19 22:16:13 +0900

    확장 기능
    1. 사용자 정의 리포지토리 구현
    2. Auditing

--- data-jpa/src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -286,4 +286,9 @@ public class MemberRepositoryTest {
         //when
         List<Member> result = memberRepository.findLockByUsername("member1");
     }
+
+    @Test
+    public void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom();
+    }
 }
