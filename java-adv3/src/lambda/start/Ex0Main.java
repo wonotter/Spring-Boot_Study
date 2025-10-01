9d4fb70a49c0bd75c8eb87603598fb3e5b9ec74c
wonotter <zjaxhfl99@kumoh.ac.kr>
2025-10-02 01:28:38 +0900

    실전 자바 - 고급 3편
    프로젝트 설정, 람다가 필요한 이유1

---
+++ java-adv3/src/lambda/start/Ex0Main.java
@@ -0,0 +1,26 @@
+package lambda.start;
+
+public class Ex0Main {
+    public static void helloJava() {
+        System.out.println("프로그램 시작"); // 변하지 않는 부분
+        System.out.println("Hello Java"); // 변하는 부분
+        System.out.println("프로그램 종료"); // 변하지 않는 부분
+    }
+
+    public static void helloSpring() {
+        System.out.println("프로그램 시작"); // 변하지 않는 부분
+        System.out.println("Hello Spring"); // 변하는 부분
+        System.out.println("프로그램 종료"); // 변하지 않는 부분
+    }
+
+    private static void hello(String str) {
+        System.out.println("프로그램 시작");
+        System.out.println(str);
+        System.out.println("프로그램 종료");
+    }
+
+    public static void main(String[] args) {
+        hello("Hello Java");
+        hello("Hello Spring");
+    }
+}
